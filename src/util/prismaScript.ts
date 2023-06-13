79d33f375ef9aef02bcb9099a6a7701ee42e90fa
serhankileci <serhanselimkileci@gmail.com>
2023-06-13 16:31:35 +0300

    used options var

--- src/util/prismaScript.ts
+++ src/util/prismaScript.ts
@@ -1,34 +1,34 @@
-import { execa } from "execa";
+import { Options as ExecaOptions, execa } from "execa";
 import { pathExists } from "./pathExists.js";
 import { projectDir } from "./constants.js";
 import { SystemPandaError } from "./SystemPandaError.js";
 
 async function prismaScript() {
 	try {
+		const options = {
+			stdio: "inherit",
+			reject: false,
+		};
+
 		console.log("Checking for Prisma files...");
 		if (!pathExists("prisma") && !pathExists("prisma/schema.prisma")) {
 			console.log("Executing: 'prisma init'...");
-			await execa("npx", ["prisma", "init"], {
-				stdio: "inherit",
-				reject: false,
-			});
+			await execa("npx", ["prisma", "init"], options as ExecaOptions);
 		}
 
 		console.log("Executing: 'prisma migrate dev'...");
 		await execa(
 			"npx",
 			["prisma", "migrate", "dev", `--schema=${projectDir}/prisma/schema.prisma`],
-			{
-				stdio: "inherit",
-				reject: false,
-			}
+			options as ExecaOptions
 		);
 
 		console.log("Executing: 'prisma generate'...");
-		await execa("npx", ["prisma", "generate", `--schema=${projectDir}/prisma/schema.prisma`], {
-			stdio: "inherit",
-			reject: false,
-		});
+		await execa(
+			"npx",
+			["prisma", "generate", `--schema=${projectDir}/prisma/schema.prisma`],
+			options as ExecaOptions
+		);
 	} catch (err) {
 		throw new SystemPandaError({ level: "error", message: JSON.stringify(err) });
 	}
