4cb3ca6e401aec2dde750b174fe47a88aff3700b
serhankileci <serhanselimkileci@gmail.com>
2023-06-13 16:33:08 +0300

    changed imports

--- src/middlewares/errHandler.ts
+++ src/middlewares/errHandler.ts
@@ -1,16 +1,22 @@
-import prisma from "@prisma/client";
-const { Prisma } = prisma;
-
-import { SystemPandaError, logfile, logger } from "../util/index.js";
+import {
+	SystemPandaError,
+	logfile,
+	logger,
+	PrismaClientInitializationError,
+	PrismaClientKnownRequestError,
+	PrismaClientRustPanicError,
+	PrismaClientUnknownRequestError,
+	PrismaClientValidationError,
+} from "../util/index.js";
 import { ErrorRequestHandler } from "express";
 
 const errHandler: ErrorRequestHandler = async (err, req, res, next) => {
 	const isPrismaErr = [
-		Prisma.PrismaClientInitializationError,
-		Prisma.PrismaClientKnownRequestError,
-		Prisma.PrismaClientRustPanicError,
-		Prisma.PrismaClientUnknownRequestError,
-		Prisma.PrismaClientValidationError,
+		PrismaClientInitializationError,
+		PrismaClientKnownRequestError,
+		PrismaClientRustPanicError,
+		PrismaClientUnknownRequestError,
+		PrismaClientValidationError,
 	].some(x => err instanceof x);
 
 	if (!res.headersSent) res.status(500).json(isPrismaErr ? err.message : err);
