4d1524890e19a62239dc5cd4ebeeee95d51b5fb7
serhankileci <serhanselimkileci@gmail.com>
2023-06-15 00:50:01 +0300

    conditional default user, rm ts-ignore comments

--- src/lib/collection/databaseSeed.ts
+++ src/lib/collection/databaseSeed.ts
@@ -1,45 +1,24 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import { randomUUID } from "crypto";
-import { selectOnQuery } from "../../util/selectOnQuery.js";
+import { selectOnQuery } from "../../util/index.js";
 
 async function databaseSeed(prisma: PrismaClient) {
-	const stdlibPlugins = [{}];
-	const internalTables = Object.keys(prisma).filter(x => !x.startsWith("$"));
+	// await prisma.systemPandaSettings.createMany();
+	// await prisma.systemPandaPlugins.createMany();
 
-	if (!internalTables.includes("systemPandaSettings")) {
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore
-		// await prisma.systemPandaSettings.createMany({
-		// 	data: [],
-		// });
-	}
-
-	if (!internalTables.includes("systemPandaPlugins")) {
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore
-		// await prisma.systemPandaPlugins.createMany({
-		// 	data: [],
-		// });
-	}
-
-	if (!internalTables.includes("users")) {
-		const saltRounds = 10;
+	if ((await prisma.users.count().length) === 0) {
 		const pw = randomUUID();
-		const salt = await bcrypt.genSalt(saltRounds);
-		const hash = await bcrypt.hash(pw, salt);
-
+		const hash = await bcrypt.hash(pw, await bcrypt.genSalt(10));
 		const userData = {
 			email: "admin@systempanda.com",
-			// name: "Admin",
-			// password: hash,
+			name: "Admin",
+			password: hash,
 		};
-
 		await prisma.users.create({
 			data: userData,
 			select: selectOnQuery(userData, "*"),
 		});
-
 		console.log(
 			`Your initial auto-generated password is: ${hash}, you should change it. It was also written to "system-panda.log".`
 		);
