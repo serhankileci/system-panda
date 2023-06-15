e485b1b65de94b043b588af1b1bd68e1bf4766d6
serhankileci <serhanselimkileci@gmail.com>
2023-06-15 23:50:06 +0300

    modularized

--- src/lib/collection/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./makePrismaModel.js";
-export * from "./databaseSeed.js";
-export * from "./defaultCollections.js";
-export * from "./mapQuery.js";
--- src/lib/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./collection/index.js";
-export * from "./Plugin.js";
-export * from "./Webhook.js";
