33988bc61664e41cc67fc5a33af112b7515f76f6
serhankileci <serhanselimkileci@gmail.com>
2023-06-16 23:10:26 +0300

    fixed query count check, logged initial user data to logfile

--- src/collection/databaseSeed.ts
+++ src/collection/databaseSeed.ts
@@ -1,13 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import { randomUUID } from "crypto";
-import { selectOnQuery } from "../util/index.js";
+import { SystemPandaError, logfile, logger, selectOnQuery } from "../util/index.js";
 
 async function databaseSeed(prisma: PrismaClient) {
 	// await prisma.systemPandaSettings.createMany();
 	// await prisma.systemPandaPlugins.createMany();
 
-	if ((await prisma.users.count().length) === 0) {
+	if ((await prisma.users.count()) === 0) {
 		const pw = randomUUID();
 		const hash = await bcrypt.hash(pw, await bcrypt.genSalt(10));
 		const userData = {
@@ -19,6 +19,14 @@ async function databaseSeed(prisma: PrismaClient) {
 			data: userData,
 			select: selectOnQuery(userData, "*"),
 		});
+
+		await logger(
+			logfile,
+			new SystemPandaError({
+				level: "informative",
+				message: `Initial user created. Email: admin@systempanda.com, password: ${hash}.`,
+			})
+		);
 		console.log(
 			`Your initial auto-generated password is: ${hash}, you should change it. It was also written to "system-panda.log".`
 		);
