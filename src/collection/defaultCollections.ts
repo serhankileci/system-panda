ec0745b047d31cd99abfd8b2022b8cfe6239c36a
serhankileci <serhanselimkileci@gmail.com>
2023-06-16 23:11:00 +0300

    adapted the change to literal Prisma types

--- src/collection/defaultCollections.ts
+++ src/collection/defaultCollections.ts
@@ -3,34 +3,34 @@ import { Collection } from "../util/index.js";
 const defaultCollections: Record<"users" | "plugins" | "settings", Collection> = {
 	users: {
 		fields: {
-			email: { required: true, type: "text", unique: true, index: true },
-			name: { required: true, type: "text" },
-			password: { required: true, type: "text" },
+			email: { required: true, type: "String", unique: true, index: true },
+			name: { required: true, type: "String" },
+			password: { required: true, type: "String" },
 		},
 	},
 	plugins: {
 		fields: {
-			title: { required: true, type: "text", unique: true },
-			author: { required: true, type: "text" },
-			version: { required: true, type: "text" },
-			sourceCode: { required: true, type: "text" },
-			active: { required: true, type: "boolean" },
+			title: { required: true, type: "String", unique: true },
+			author: { required: true, type: "String" },
+			version: { required: true, type: "String" },
+			sourceCode: { required: true, type: "String" },
+			active: { required: true, type: "Boolean" },
 		},
 	},
 	settings: {
 		fields: {
 			name: {
-				type: "text",
+				type: "String",
 				required: true,
 				unique: true,
 			},
 			value: {
-				type: "text",
+				type: "String",
 				required: true,
 				defaultValue: "null",
 			},
 			type: {
-				type: "text",
+				type: "String",
 			},
 		},
 	},
