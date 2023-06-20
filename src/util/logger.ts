4a502a452e9800afcd2dc566799a8afe10066888
serhankileci <serhanselimkileci@gmail.com>
2023-06-21 02:02:02 +0300

    log to stdout if true

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,7 +1,9 @@
 import { appendFile } from "fs/promises";
 import { writeOrAppend, SystemPandaError } from "./index.js";
 
-async function logger(path: string, err: SystemPandaError | Error) {
+async function logger(path: string, err: SystemPandaError | Error, stdout = true) {
+	if (stdout) console.error(err);
+
 	const date = new Date(Number(Date.now())).toLocaleDateString();
 	const hour = new Date(date).toLocaleString("default", {
 		hour: "2-digit",
