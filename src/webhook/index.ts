8d13b98837fbff55af50ffc5aa198ca004e8d25a
serhankileci <serhanselimkileci@gmail.com>
2023-07-03 16:58:04 +0300

    fn declaration to expr

--- src/webhook/index.ts
+++ src/webhook/index.ts
@@ -1,11 +1,11 @@
 import EventEmitter from "events";
-import { crudMapping, EventTriggerPayload, Webhook, WebhookOperations } from "../util/index.js";
+import { crudMapping, EventTriggerPayload, WebhookFn } from "../util/index.js";
 const emitter = new EventEmitter();
 const defaultHeaders = {
 	"content-type": "application/json",
 };
 
-function webhook(webhook: Webhook): WebhookOperations {
+const webhook: WebhookFn = webhook => {
 	const { api, name, headers, onOperation } = webhook;
 
 	return {
@@ -27,6 +27,6 @@ function webhook(webhook: Webhook): WebhookOperations {
 			if (onOperation.some(x => crudMapping[x])) emitter.emit(name, data);
 		},
 	};
-}
+};
 
 export { webhook };
