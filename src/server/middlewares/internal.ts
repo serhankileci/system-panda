0ef15d51b193f822f9b1f732da771cd8744f5dba
serhankileci <serhanselimkileci@gmail.com>
2023-07-05 23:25:47 +0300

    moved loading sessiondata fn to here

--- src/server/middlewares/internal.ts
+++ src/server/middlewares/internal.ts
@@ -1,17 +1,48 @@
 import { Request, Response, NextFunction, static as serveStatic } from "express";
-import { Context, packageProjectDir } from "../../util/index.js";
+import {
+	AuthSession,
+	Context,
+	SESSION_COOKIE_NAME,
+	filterObjByKeys,
+	packageProjectDir,
+} from "../../util/index.js";
 import path from "node:path";
 
-function internalMiddlewares(ctx: Context) {
-	const loadCtxWithExpress = (req: Request, res: Response, next: NextFunction) => {
-		if (!ctx.express) ctx.express = { req, res };
-		next();
+function internalMiddlewares(ctx: Context, authSession: AuthSession) {
+	const loadCtxWithData = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			if (!ctx.express) ctx.express = { req, res };
+
+			if (req.cookies[SESSION_COOKIE_NAME]) {
+				const data = await ctx.prisma.systemPandaSession.findUnique({
+					where: {
+						id: req.cookies[SESSION_COOKIE_NAME],
+					},
+					include: {
+						relation_users: true,
+					},
+				});
+
+				if (authSession.sessionData === "*") {
+					ctx.sessionData = data.relation_users;
+				} else if (
+					Array.isArray(authSession.sessionData) &&
+					authSession.sessionData.length > 0
+				) {
+					ctx.sessionData = filterObjByKeys(data.relation_users, authSession.sessionData);
+				}
+			}
+
+			next();
+		} catch (err) {
+			next(err);
+		}
 	};
 	const systemPandaStatic = serveStatic(path.join(packageProjectDir, "system-panda-static"), {
 		extensions: ["html"],
 	});
 
-	return [loadCtxWithExpress, systemPandaStatic];
+	return [loadCtxWithData, systemPandaStatic];
 }
 
 export { internalMiddlewares };
