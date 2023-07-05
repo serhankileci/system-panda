08f69ea2c6e030b83f2a61c1653fb52f21016156
serhankileci <serhanselimkileci@gmail.com>
2023-07-05 19:47:16 +0300

    moved to dedicated file, using this file as barrel module

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,34 +1,5 @@
-import path from "node:path";
-import { mkdir, writeFile } from "node:fs/promises";
-import { Collections, Database, pathExists, userProjectDir } from "../util/index.js";
-import { databaseSeed } from "../collection/index.js";
-import { execPrismaScripts } from "./execPrismaScripts.js";
-import { getModels } from "./getModels.js";
-import { makePrismaModel } from "./makePrismaModel.js";
-
-async function database(db: Database, collections: Collections) {
-	const generatedSchemaString = await makePrismaModel(db, collections);
-
-	if (!(await pathExists(`${userProjectDir}/prisma`))) await mkdir(`${userProjectDir}/prisma`);
-	await writeFile(path.resolve(`${userProjectDir}/prisma/schema.prisma`), generatedSchemaString);
-	await execPrismaScripts();
-
-	const models = getModels(generatedSchemaString);
-
-	console.log("🐼 Connecting to database...");
-	const { PrismaClient } = await import("@prisma/client");
-	const prisma = new PrismaClient({
-		errorFormat: db.errorFormat,
-		log: db.log,
-		datasources: { db: { url: db.URI } },
-	});
-
-	await databaseSeed(prisma);
-
-	return {
-		prisma,
-		models,
-	};
-}
-
-export { database };
+export * from "./database.js";
+export * from "./execPrismaScripts.js";
+export * from "./getModels.js";
+export * from "./prismaSessionStore.js";
+export * from "./makePrismaModel.js";
