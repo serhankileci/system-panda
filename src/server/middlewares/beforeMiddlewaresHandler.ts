d642a28192ad35f6f97b402fae5ec82fda8cdac1
serhankileci <serhanselimkileci@gmail.com>
2023-07-05 22:51:25 +0300

    cookie parser and session middlewares

--- src/server/middlewares/beforeMiddlewaresHandler.ts
+++ src/server/middlewares/beforeMiddlewaresHandler.ts
@@ -4,9 +4,23 @@ import { rateLimit } from "express-rate-limit";
 import helmet from "helmet";
 import compression from "compression";
 import cors from "cors";
-import { DefaultMiddlewares, MiddlewareHandler } from "../../util/index.js";
+import {
+	AuthSession,
+	DefaultMiddlewares,
+	MiddlewareHandler,
+	SESSION_COOKIE_NAME,
+} from "../../util/index.js";
+import session from "express-session";
+import { randomUUID } from "crypto";
+import cookieParser from "cookie-parser";
+import { PrismaClient } from "@prisma/client";
+import { PrismaSessionStore } from "../../db/index.js";
 
-const beforeMiddlewaresHandler = (defaultMiddlewares: DefaultMiddlewares): MiddlewareHandler[] => {
+const beforeMiddlewaresHandler = (
+	defaultMiddlewares: DefaultMiddlewares,
+	authSession: AuthSession,
+	prisma: PrismaClient
+): MiddlewareHandler[] => {
 	const {
 		compression: compressionOpt,
 		cors: corsOpt,
@@ -17,13 +31,27 @@ const beforeMiddlewaresHandler = (defaultMiddlewares: DefaultMiddlewares): Middl
 		serveStatic: serveStaticOpt,
 		urlencoded: urlencodedOpt,
 	} = defaultMiddlewares || {};
+	const { secret, maxAge } = authSession.options;
 
 	const middlewares = [
 		helmet(helmetOpt || {}),
 		json(jsonOpt || {}),
 		urlencoded(urlencodedOpt || { extended: false }),
+		cookieParser(),
 		compression(compressionOpt || {}),
 		cors(corsOpt || {}),
+		session({
+			name: SESSION_COOKIE_NAME,
+			store: new PrismaSessionStore(prisma),
+			secret: secret || randomUUID(),
+			cookie: {
+				httpOnly: true,
+				maxAge: maxAge || 24 * 60 * 60 * 1000,
+				secure: process.env.NODE_ENV === "production",
+			},
+			saveUninitialized: false,
+			resave: false,
+		}),
 	];
 
 	if (morganOpt) middlewares.push(morgan(morganOpt.format, morganOpt.options));
