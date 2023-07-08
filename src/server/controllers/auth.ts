d86dd91f2352c9bf52ff924f620a24bb69d40454
serhankileci <serhanselimkileci@gmail.com>
2023-07-08 23:35:19 +0300

    returning middleware instead of func that returns middleware, getting fresh data and keys from the store, misc.

--- src/server/controllers/auth.ts
+++ src/server/controllers/auth.ts
@@ -1,52 +1,49 @@
-import { PrismaClient } from "@prisma/client";
-import { NormalizedAuthFields, SESSION_COOKIE_NAME } from "../../util/index.js";
+import { SESSION, getDataStore } from "../../util/index.js";
 import { NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt";
 
-const authenticate = (prisma: PrismaClient, normalizedAuthFields: NormalizedAuthFields) => {
-	const { collectionKey, secretField, uniqueIdentifierField } = normalizedAuthFields;
+const authenticate = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const { prisma, authFields } = getDataStore();
+		const { collectionKey, secretField, uniqueIdentifierField } = authFields;
+		const { httpOnly, secure, originalMaxAge } = req.session.cookie;
 
-	return async (req: Request, res: Response, next: NextFunction) => {
-		try {
-			const { httpOnly, secure, originalMaxAge } = req.session.cookie;
+		const data = await prisma[collectionKey].findUnique({
+			where: {
+				[uniqueIdentifierField]: req.body[uniqueIdentifierField],
+			},
+		});
 
-			const data = await prisma[collectionKey].findUnique({
-				where: {
-					[uniqueIdentifierField]: req.body[uniqueIdentifierField],
-				},
-			});
-
-			if (!data) {
-				res.status(404);
-				throw "User not found.";
-			}
-
-			const isPasswordMatch = await bcrypt.compare(req.body[secretField], data[secretField]);
-
-			if (!isPasswordMatch) {
-				res.status(400);
-				throw "Invalid password.";
-			}
-
-			req.sessionStore.set(
-				req.session.id,
-				{ cookie: req.session.cookie, userID: data.id },
-				err => {
-					if (err) next(err);
-
-					res.cookie(SESSION_COOKIE_NAME, req.session.id, {
-						maxAge: Number(originalMaxAge),
-						secure: Boolean(secure),
-						httpOnly,
-					});
-
-					return res.redirect("/");
-				}
-			);
-		} catch (err) {
-			next(err);
+		if (!data) {
+			res.status(404);
+			throw `User ${req.body[uniqueIdentifierField]} not found.`;
 		}
-	};
+
+		const isPasswordMatch = await bcrypt.compare(req.body[secretField], data[secretField]);
+
+		if (!isPasswordMatch) {
+			res.status(400);
+			throw "Invalid password.";
+		}
+
+		req.sessionStore.set(
+			req.session.id,
+			{ cookie: req.session.cookie, userID: data.id },
+			err => {
+				if (err) next(err);
+
+				res.cookie(SESSION.COOKIE_NAME, req.session.id, {
+					maxAge: Number(originalMaxAge),
+					secure: Boolean(secure),
+					httpOnly,
+				});
+
+				return res.redirect("/");
+			}
+		);
+	} catch (err) {
+		next(err);
+	}
 };
 
 export { authenticate };
