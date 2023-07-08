375dfad47bea6391633780699c0af27ac970923c
serhankileci <serhanselimkileci@gmail.com>
2023-07-08 23:16:06 +0300

    returning the router directly, setting the plugins to the data store instead of mutableProps

--- src/server/routers/plugins.ts
+++ src/server/routers/plugins.ts
@@ -1,78 +1,92 @@
 import express from "express";
-import { MutableProps, PLUGINS_API } from "../../util/index.js";
-import { plugin } from "../../plugin/index.js";
-import { PrismaClient } from "@prisma/client";
-const router = express.Router();
+import { PLUGINS_API, setDataStore } from "../../util/index.js";
+import { plugins } from "../../plugins/index.js";
+const pluginsRouter = express.Router();
 
-const pluginsRouter = (mutableProps: MutableProps, prisma: PrismaClient) => {
-	return router
-		.get("/", async (req, res, next) => {
-			try {
-				return res.json(await (await fetch(PLUGINS_API)).json());
-			} catch (err) {
-				next(err);
-			}
-		})
-		.get("/:title?", async (req, res, next) => {
-			try {
-				const { title } = req.params;
-				return res.json(await (await fetch(`${PLUGINS_API}/${title}`)).json());
-			} catch (err) {
-				next(err);
-			}
-		})
-		.get("/:title/install", async (req, res, next) => {
-			try {
-				const { title } = req.params;
-				await plugin(prisma).install(title);
-				mutableProps.plugins = await plugin(prisma).load();
+pluginsRouter
+	.get("/", async (req, res, next) => {
+		try {
+			return res.json(await (await fetch(PLUGINS_API)).json());
+		} catch (err) {
+			next(err);
+		}
+	})
+	.get("/:title?", async (req, res, next) => {
+		try {
+			const { title } = req.params;
 
-				return res.json({
-					message: `Installed plugin: ${title}.`,
-				});
-			} catch (err) {
-				next(err);
-			}
-		})
-		.get("/:title/uninstall", async (req, res, next) => {
-			try {
-				const { title } = req.params;
-				await plugin(prisma).uninstall(title);
-				mutableProps.plugins = await plugin(prisma).load();
+			return res.json(await (await fetch(`${PLUGINS_API}/${title}`)).json());
+		} catch (err) {
+			next(err);
+		}
+	})
+	.get("/:title/install", async (req, res, next) => {
+		try {
+			const { title } = req.params;
 
-				return res.json({
-					message: `Uninstalled plugin: ${title}.`,
-				});
-			} catch (err) {
-				next(err);
-			}
-		})
-		.get("/:title/enable", async (req, res, next) => {
-			try {
-				const { title } = req.params;
-				await plugin(prisma).enable(title);
-				mutableProps.plugins = await plugin(prisma).load();
+			await plugins.install(title);
 
-				return res.json({
-					message: `Enabled plugin: ${title} and reloaded the plugins.`,
-				});
-			} catch (err) {
-				next(err);
-			}
-		})
-		.get("/:title/disable", async (req, res, next) => {
-			try {
-				const { title } = req.params;
-				await plugin(prisma).disable(title);
-				mutableProps.plugins = await plugin(prisma).load();
+			setDataStore({
+				pluginStore: await plugins.load(),
+			});
 
-				return res.json({
-					message: `Disabled plugin: ${title} and reloaded the plugins.`,
-				});
-			} catch (err) {
-				next(err);
-			}
-		});
-};
+			return res.json({
+				message: `Installed plugin: ${title}.`,
+			});
+		} catch (err) {
+			next(err);
+		}
+	})
+	.get("/:title/uninstall", async (req, res, next) => {
+		try {
+			const { title } = req.params;
+
+			await plugins.uninstall(title);
+
+			setDataStore({
+				pluginStore: await plugins.load(),
+			});
+
+			return res.json({
+				message: `Uninstalled plugin: ${title}.`,
+			});
+		} catch (err) {
+			next(err);
+		}
+	})
+	.get("/:title/enable", async (req, res, next) => {
+		try {
+			const { title } = req.params;
+
+			await plugins.enable(title);
+
+			setDataStore({
+				pluginStore: await plugins.load(),
+			});
+
+			return res.json({
+				message: `Enabled plugin: ${title} and reloaded the plugins().`,
+			});
+		} catch (err) {
+			next(err);
+		}
+	})
+	.get("/:title/disable", async (req, res, next) => {
+		try {
+			const { title } = req.params;
+
+			await plugins.disable(title);
+
+			setDataStore({
+				pluginStore: await plugins.load(),
+			});
+
+			return res.json({
+				message: `Disabled plugin: ${title} and reloaded the plugins().`,
+			});
+		} catch (err) {
+			next(err);
+		}
+	});
 
 export { pluginsRouter };
