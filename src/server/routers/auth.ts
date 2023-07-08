46efc149a3715cf9466b6fdc351452fb133e76c0
serhankileci <serhanselimkileci@gmail.com>
2023-07-08 23:32:15 +0300

    returning the router instead of func, using const session vars, removed redundant args

--- src/server/routers/auth.ts
+++ src/server/routers/auth.ts
@@ -1,32 +1,30 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
 import { ifAuthenticated, ifNotAuthenticated } from "../middlewares/index.js";
-import { NormalizedAuthFields, SESSION_COOKIE_NAME } from "../../util/index.js";
+import { SESSION } from "../../util/index.js";
 import { authenticate } from "../controllers/index.js";
-const router = express.Router();
+const authRouter = express.Router();
 
-const authRouter = (prisma: PrismaClient, normalizedAuthFields: NormalizedAuthFields) => {
-	return router
-		.get("/login", ifNotAuthenticated, (req, res) =>
-			res.render("auth", {
-				title: "SystemPanda - Login",
-				page: "login",
-			})
-		)
-		.post("/login", ifNotAuthenticated, authenticate(prisma, normalizedAuthFields))
-		.get("/logout", ifAuthenticated, (req, res) => {
-			res.render("auth", {
-				title: "SystemPanda - Logout",
-				page: "logout",
-			});
+authRouter
+	.get("/login", ifNotAuthenticated, (req, res) =>
+		res.render("auth", {
+			title: "SystemPanda - Login",
+			page: "login",
 		})
-		.post("/logout", ifAuthenticated, (req, res, next) => {
-			req.sessionStore.destroy(req.cookies[SESSION_COOKIE_NAME], err => {
-				if (err) next(err);
-				res.clearCookie(SESSION_COOKIE_NAME);
-				res.send("Logged out.");
-			});
+	)
+	.post("/login", ifNotAuthenticated, authenticate)
+	.get("/logout", ifAuthenticated, (req, res) => {
+		res.render("auth", {
+			title: "SystemPanda - Logout",
+			page: "logout",
 		});
-};
+	})
+	.post("/logout", ifAuthenticated, (req, res, next) => {
+		req.sessionStore.destroy(req.cookies[SESSION.COOKIE_NAME], err => {
+			if (err) next(err);
+
+			res.clearCookie(SESSION.COOKIE_NAME);
+			res.send("Logged out.");
+		});
+	});
 
 export { authRouter };
