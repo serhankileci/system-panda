5b2ff8fd6f153e6ecd10b16167416c85ba0517a8
serhankileci <serhanselimkileci@gmail.com>
2023-07-08 23:30:37 +0300

    using vars from the store

---
+++ src/database/seed.ts
@@ -0,0 +1,32 @@
+import bcrypt from "bcrypt";
+import { selectOnQuery, getDataStore } from "../util/index.js";
+
+async function seed() {
+	const { prisma, initFirstAuth, authFields } = getDataStore();
+	const { collectionKey, secretField } = authFields;
+
+	// await prisma.systemPandaSettings.createMany();
+	// await prisma.systemPandaPlugins.createMany();
+
+	if ((await prisma[collectionKey].count()) === 0) {
+		console.log("🐼 Creating initial user...");
+
+		const hash = await bcrypt.hash(
+			initFirstAuth && initFirstAuth[secretField],
+			await bcrypt.genSalt(10)
+		);
+		const data = {
+			...initFirstAuth,
+			[secretField]: hash,
+		};
+
+		await prisma[collectionKey].create({
+			data,
+			select: selectOnQuery(data, "*"),
+		});
+
+		console.log("🐼 Initial user created with the specified credentials.");
+	}
+}
+
+export { seed };
