e41c30d02d8f215397df4d243f99cd4459909c95
serhankileci <serhanselimkileci@gmail.com>
2023-07-11 19:42:43 +0300

    not writing or importing plugins from the file system anymore, importing base64 encoded js func strings, removed fs operations, pascalCase to snake_case SystemPanda tables

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,40 +1,39 @@
 import {
 	PLUGINS_API,
-	pluginsDir,
 	DatabasePlugin,
 	ActiveInactivePlugins,
-	pathExists,
 	Plugins,
 	getDataStore,
 } from "../util/index.js";
-import { mkdir, rm, writeFile } from "fs/promises";
-
-if (!(await pathExists(pluginsDir))) await mkdir(pluginsDir);
 
 const plugins: Plugins = {
 	prisma: () => getDataStore().prisma,
 	load: async function () {
-		const dbPlugins: DatabasePlugin[] = await this.prisma().systemPandaPlugins.findMany();
+		const dbPlugins: DatabasePlugin[] = await this.prisma().system_panda_plugins.findMany();
 		const obj: ActiveInactivePlugins = { active: [], inactive: [] };
 
 		for (const plugin of dbPlugins) {
-			const fn = (await import(`${pluginsDir}/${plugin.title}.js`)).default;
+			const defaultExportFromFuncString = await import(
+				"data:application/javascript;base64," +
+					Buffer.from(plugin.sourceCode.toString()).toString("base64")
+			).then(x => x.default);
+
 			obj[plugin.active ? "active" : "inactive"].push({
 				...plugin,
-				fn,
+				sourceCode: defaultExportFromFuncString,
 			});
 		}
 
 		return obj;
 	},
 	enable: async function (title) {
-		await this.prisma().systemPandaPlugins.update({
+		await this.prisma().system_panda_plugins.update({
 			where: { title },
 			data: { active: true },
 		});
 	},
 	disable: async function (title) {
-		await this.prisma().systemPandaPlugins.update({
+		await this.prisma().system_panda_plugins.update({
 			where: { title },
 			data: { active: false },
 		});
@@ -42,16 +41,12 @@ const plugins: Plugins = {
 	install: async function (title) {
 		const res = await (await fetch(`${PLUGINS_API}/${title}`)).json();
 
-		await this.prisma().systemPandaPlugins.create({
+		await this.prisma().system_panda_plugins.create({
 			data: { ...res, active: false },
 		});
-
-		await writeFile(`${pluginsDir}/${title}.js`, res.sourceCode);
 	},
 	uninstall: async function (title) {
-		await this.prisma().systemPandaPlugins.delete({ where: { title } });
-
-		await rm(`${pluginsDir}/${title}.js`);
+		await this.prisma().system_panda_plugins.delete({ where: { title } });
 	},
 };
 
