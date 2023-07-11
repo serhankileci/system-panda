556b3b5a0ddcdd83f50772eb180741ac3b68874b
serhankileci <serhanselimkileci@gmail.com>
2023-07-11 19:45:05 +0300

    template for handling both internal (SystemPanda specific) and external (prisma) scripts

--- src/util/cli.ts
+++ src/util/cli.ts
@@ -1,15 +1,27 @@
 #!/usr/bin/env node
 import { execa } from "execa";
-import { SystemPandaError, logfile, logger } from "./index.js";
+import { logfile, logger } from "./index.js";
 
 (async () => {
 	try {
-		await execa("prisma", process.argv.slice(process.argv.indexOf("system-panda")), {
+		const cmds: { int: string[]; ext: string[] } = {
+			int: [],
+			ext: ["prisma"],
+		};
+		const [bin, ...rest] = process.argv.slice(2);
+		const options = {
 			stdio: "inherit",
 			reject: false,
-		});
+		} as const;
+
+		if (cmds.ext.includes(bin)) {
+			await execa(bin, rest, options);
+		} else if (cmds.int.includes(bin)) {
+			// internal scripts for later
+		} else {
+			throw "Command not found.";
+		}
 	} catch (err: unknown) {
-		console.log(err);
-		await logger(logfile, err as SystemPandaError | Error);
+		await logger(logfile, err as Error);
 	}
 })();
