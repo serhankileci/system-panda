c5372c2a3f2e71a0963ce8e558bf157c3824164b
serhankileci <serhanselimkileci@gmail.com>
2023-07-15 13:32:05 +0300

    floating promises/unsafe assignments off

--- src/ui/.eslintrc.cjs
+++ src/ui/.eslintrc.cjs
@@ -20,5 +20,7 @@ module.exports = {
 	rules: {
 		"react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
 		"@typescript-eslint/no-non-null-assertion": "off",
+		"@typescript-eslint/no-floating-promises": "off",
+		"@typescript-eslint/no-unsafe-assignment": "off",
 	},
 };
