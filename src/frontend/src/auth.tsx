448ed0384570fc82e37675f02673865cc4879137
serhankileci <serhanselimkileci@gmail.com>
2023-07-16 11:47:43 +0300

    auth components

---
+++ src/frontend/src/auth.tsx
@@ -0,0 +1,69 @@
+import { FormEvent } from "react";
+import { baseURL } from "./util.ts";
+
+const auth = {
+	Login: function ({ setLoggedIn }: { setLoggedIn: (bool: boolean) => void }) {
+		const handleForm = async (e: FormEvent<HTMLFormElement>) => {
+			e.preventDefault();
+
+			const res = await fetch(baseURL + "/system-panda-api/auth/login", {
+				method: "post",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(Object.fromEntries(new FormData(e.currentTarget))),
+			});
+
+			if (res.ok) {
+				setLoggedIn(true);
+				localStorage.setItem("logged_in", "true");
+			}
+		};
+
+		return (
+			<form
+				onSubmit={e => {
+					handleForm(e);
+				}}
+			>
+				<div>
+					<label htmlFor="email">Email</label>
+					<input type="email" id="email" name="email" required />
+				</div>
+				<div>
+					<label htmlFor="password">Password</label>
+					<input type="password" id="password" name="password" required />
+				</div>
+				<div>
+					<button type="submit">Login</button>
+				</div>
+			</form>
+		);
+	},
+	Logout: function ({ setLoggedIn }: { setLoggedIn: (bool: boolean) => void }) {
+		const handleForm = async (e: FormEvent<HTMLFormElement>) => {
+			e.preventDefault();
+
+			const res = await fetch(baseURL + "/system-panda-api/auth/logout", {
+				method: "post",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(Object.fromEntries(new FormData(e.currentTarget))),
+			});
+
+			if (res.ok) {
+				setLoggedIn(false);
+				localStorage.removeItem("logged_in");
+			}
+		};
+
+		return (
+			<form
+				onSubmit={e => {
+					handleForm(e);
+				}}
+			>
+				<button type="submit">Logout</button>
+			</form>
+		);
+	},
+};
+
+export { auth };
