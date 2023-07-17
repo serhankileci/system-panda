bb48ff8892c9f65be17999ab182f3aa15be6aea0
serhankileci <serhanselimkileci@gmail.com>
2023-07-17 20:07:00 +0300

    removed get routes

--- src/server/routers/auth.ts
+++ src/server/routers/auth.ts
@@ -5,26 +5,18 @@ import { authenticate } from "../controllers/index.js";
 const authRouter = express.Router();
 
 authRouter
-	.get("/login", ifNotAuthenticated, (req, res) =>
-		res.render("auth", {
-			title: "SystemPanda - Login",
-			page: "login",
-		})
-	)
 	.post("/login", ifNotAuthenticated, authenticate)
-	.get("/logout", ifAuthenticated, (req, res) => {
-		res.render("auth", {
-			title: "SystemPanda - Logout",
-			page: "logout",
-		});
-	})
 	.post("/logout", ifAuthenticated, (req, res, next) => {
-		req.sessionStore.destroy(req.cookies[SESSION.COOKIE_NAME], err => {
-			if (err) next(err);
+		try {
+			req.sessionStore.destroy(req.cookies[SESSION.COOKIE_NAME], err => {
+				if (err) next(err);
 
-			res.clearCookie(SESSION.COOKIE_NAME);
-			res.send("Logged out.");
-		});
+				res.clearCookie(SESSION.COOKIE_NAME);
+				res.sendStatus(200);
+			});
+		} catch (err) {
+			next(err);
+		}
 	});
 
 export { authRouter };
