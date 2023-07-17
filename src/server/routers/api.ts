5b7461d1a2ae87ee343f20b7b97f8d2bfd5766c6
serhankileci <serhanselimkileci@gmail.com>
2023-07-17 20:07:46 +0300

    api router

---
+++ src/server/routers/api.ts
@@ -0,0 +1,54 @@
+import express from "express";
+import { ifAuthenticated } from "../middlewares/index.js";
+import { Context, Models, Webhook, getConfigStore, getDataStore } from "../../util/index.js";
+import { collection } from "../controllers/index.js";
+import { authRouter } from "./auth.js";
+import { pluginsRouter } from "./plugins.js";
+import { webhook } from "../../webhooks/index.js";
+const apiRouter = express.Router();
+
+function apiHandler(ctx: Context, globalWebhooks: Webhook[], models: Models) {
+	const {
+		settings: { healthCheck },
+		content: { collections },
+	} = getConfigStore();
+	const { prisma } = getDataStore();
+
+	apiRouter
+		// ...
+		.use("/auth", authRouter)
+		.use("/plugins", pluginsRouter);
+
+	if (healthCheck !== false)
+		apiRouter.get(healthCheck?.path || "/health-check", (req, res) => {
+			res.json(
+				healthCheck?.data || {
+					status: "healthy",
+					timestamp: new Date().toISOString(),
+					uptime: process.uptime(),
+				}
+			);
+		});
+
+	apiRouter.get("/collections", (req, res) =>
+		res.json(Object.entries(collections).map(([k, v]) => "/" + (v.slug || k)))
+	);
+
+	for (const [cKey, cValue] of Object.entries(collections)) {
+		const { hooks, slug, webhooks } = cValue;
+		const slugOrKey = slug || cKey;
+		const query = prisma[cKey];
+		const mergedWebhooks = [...(globalWebhooks || []), ...(webhooks || [])];
+
+		mergedWebhooks?.forEach(obj => webhook(obj).init());
+
+		apiRouter.all(
+			`/collections/${slugOrKey}`,
+			collection(query, ctx, hooks, models, mergedWebhooks, cKey, slugOrKey)
+		);
+	}
+
+	return apiRouter;
+}
+
+export { apiHandler };
