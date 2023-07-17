df5d67de9f708f5eca198b73d473e0e4b0401a37
serhankileci <serhanselimkileci@gmail.com>
2023-07-17 20:06:48 +0300

    try/catch

--- src/server/middlewares/auth.ts
+++ src/server/middlewares/auth.ts
@@ -2,21 +2,29 @@ import { NextFunction, Request, Response } from "express";
 import { SESSION } from "../../util/index.js";
 
 function ifAuthenticated(req: Request, res: Response, next: NextFunction) {
-	req.sessionStore.get(req.cookies[SESSION.COOKIE_NAME], (err, session) => {
-		if (err) return next(err);
+	try {
+		req.sessionStore.get(req.cookies[SESSION.COOKIE_NAME], (err, session) => {
+			if (err) return next(err);
 
-		if (session) return next();
-		else return res.redirect("/auth/login");
-	});
+			if (session) return next();
+			else return res.sendStatus(401);
+		});
+	} catch (err) {
+		next(err);
+	}
 }
 
 function ifNotAuthenticated(req: Request, res: Response, next: NextFunction) {
-	req.sessionStore.get(req.cookies[SESSION.COOKIE_NAME], (err, session) => {
-		if (err) return next(err);
+	try {
+		req.sessionStore.get(req.cookies[SESSION.COOKIE_NAME], (err, session) => {
+			if (err) return next(err);
 
-		if (session) return res.redirect("/");
-		else return next();
-	});
+			if (session) return res.sendStatus(200);
+			else return next();
+		});
+	} catch (err) {
+		next(err);
+	}
 }
 
 export { ifAuthenticated, ifNotAuthenticated };
