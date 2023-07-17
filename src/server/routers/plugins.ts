8769552c211fa793b006dd4eacca66ebca69cb3b
serhankileci <serhanselimkileci@gmail.com>
2023-07-17 20:08:26 +0300

    separated plugins to local/remote routers along with the logic

--- src/server/routers/plugins.ts
+++ src/server/routers/plugins.ts
@@ -1,42 +1,17 @@
 import express from "express";
-import { PLUGINS_API, setDataStore } from "../../util/index.js";
+import { PLUGINS_API, getDataStore, setDataStore } from "../../util/index.js";
 import { plugins } from "../../plugins/index.js";
 const pluginsRouter = express.Router();
+const localPluginsRouter = express.Router();
+const remotePluginsRouter = express.Router();
 
 pluginsRouter
-	.get("/", async (req, res, next) => {
-		try {
-			return res.json(await (await fetch(PLUGINS_API)).json());
-		} catch (err) {
-			next(err);
-		}
-	})
-	.get("/:title?", async (req, res, next) => {
-		try {
-			const { title } = req.params;
+	// ...
+	.use("/local", localPluginsRouter)
+	.use("/remote", remotePluginsRouter);
 
-			return res.json(await (await fetch(`${PLUGINS_API}/${title}`)).json());
-		} catch (err) {
-			next(err);
-		}
-	})
-	.get("/:title/install", async (req, res, next) => {
-		try {
-			const { title } = req.params;
-
-			await plugins.install(title);
-
-			setDataStore({
-				pluginStore: await plugins.load(),
-			});
-
-			return res.json({
-				message: `Installed plugin: ${title}.`,
-			});
-		} catch (err) {
-			next(err);
-		}
-	})
+localPluginsRouter
+	.get("/", (req, res) => res.json({ plugins: getDataStore().pluginStore }))
 	.get("/:title/uninstall", async (req, res, next) => {
 		try {
 			const { title } = req.params;
@@ -81,8 +56,41 @@ pluginsRouter
 				pluginStore: await plugins.load(),
 			});
 
-			return res.json({
+			res.json({
 				message: `Disabled plugin: ${title} and reloaded the plugins().`,
+			});
+		} catch (err) {
+			next(err);
+		}
+	});
+
+remotePluginsRouter
+	.get("/", async (req, res, next) => {
+		try {
+			return res.json(await (await fetch(PLUGINS_API)).json());
+		} catch (err) {
+			next(err);
+		}
+	})
+	.get("/:title?", async (req, res, next) => {
+		try {
+			return res.json(await (await fetch(`${PLUGINS_API}/${req.params.title}`)).json());
+		} catch (err) {
+			next(err);
+		}
+	})
+	.get("/:title/install", async (req, res, next) => {
+		try {
+			const { title } = req.params;
+
+			await plugins.install(title);
+
+			setDataStore({
+				pluginStore: await plugins.load(),
+			});
+
+			return res.json({
+				message: `Installed plugin: ${title}.`,
 			});
 		} catch (err) {
 			next(err);
