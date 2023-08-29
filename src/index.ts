9c3386b85fb09bbce8f3fa1d1a92303151ab26cd
serhankileci <serhanselimkileci@gmail.com>
2023-08-29 23:01:29 +0300

    moved specific table schemas to overrideDefaultCollections func

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,41 @@
 import { database } from "./database/index.js";
-import { defaultCollections } from "./collections/index.js";
+import { overrideDefaultCollections } from "./collections/index.js";
+import { server } from "./server/index.js";
 import {
 	SystemPandaError,
 	logger,
 	logfile,
 	SP,
-	Collections,
-	getConfigStore,
 	setConfigStore,
-	dataStore,
 	setDataStore,
+	getDataStore,
 } from "./util/index.js";
-import { server } from "./server/index.js";
 
 const SystemPanda: SP = async function (options) {
 	try {
 		console.log("🐼 Building SystemPanda...");
 
 		setConfigStore(options);
-		const { content, settings } = getConfigStore();
 
-		const { system_panda_plugins, system_panda_sessions, system_panda_users } =
-			defaultCollections;
-		const { collections, webhooks: globalWebhooks } = content || {};
-		const {
-			authSession: { authFields, initFirstAuth },
-		} = settings;
+		const mergedAuthFields = {
+			...getDataStore().authFields,
+			...options.settings.authSession.authFields,
+		};
+		mergedAuthFields.relationKey = "relation_" + mergedAuthFields.collectionKey;
+
+		const [authCollection, internalCollections] = overrideDefaultCollections(mergedAuthFields);
 
 		setDataStore({
-			authFields: {
-				...authFields,
-				...dataStore.authFields,
+			authFields: mergedAuthFields,
+			initFirstAuth: options.settings.authSession.initFirstAuth,
+			normalizedCollections: {
+				visible: { ...options.content.collections, ...authCollection },
+				internal: internalCollections,
 			},
-			initFirstAuth,
 		});
 
-		const authCollection: Collections = {
-			[dataStore.authFields.collectionKey]: {
-				...system_panda_users,
-				fields: {
-					...system_panda_users.fields,
-					[dataStore.authFields.uniqueIdentifierField]: {
-						type: "String",
-						required: true,
-						unique: true,
-						index: true,
-					},
-					[dataStore.authFields.secretField]: { type: "String", required: true },
-				},
-			},
-		};
-		const internalCollections: Collections = {
-			system_panda_plugins,
-			system_panda_sessions: {
-				...system_panda_sessions,
-				fields: {
-					...system_panda_sessions.fields,
-					[`relation_${dataStore.authFields.collectionKey}`]: {
-						type: "relation",
-						many: false,
-						ref: `${dataStore.authFields.collectionKey}.id`,
-					},
-				},
-			},
-		};
-		const visibleCollections = { ...collections, ...authCollection };
-		const models = await database(settings.db, {
-			...visibleCollections,
-			...internalCollections,
-		});
-		await server(settings, visibleCollections, models, globalWebhooks);
+		await database();
+		await server();
 	} catch (err: unknown) {
 		await logger(logfile, err as SystemPandaError | Error);
 	}
